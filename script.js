23f1b7c203efa7c1712be6e6e814bc6c3d005570
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-08-28 22:19:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function generateText(){
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			resolve('Hello, World!');
+		}, 1000)
+	})
+}
+
+async function updateHTML(){
+	const text = await generateText;
+	document.getElementById('output').innerHTML = text;
+}
+
+updateHTML();
